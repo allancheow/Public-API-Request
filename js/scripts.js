3120dd15e45af2f279fa7f6f97fe41cc9cc9cf66
Allan Cheow <allan.cheow132@gmail.com>
2021-02-16 17:05:50 -0800

    added number and date formatter

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 //console.log(`I'm connected`);
 
 // Initiate variables to be used
-const randomUserURL = `https://randomuser.me/api/?results=12`;
+const randomUserURL = `https://randomuser.me/api/?nat=us&results=12`;
 const galleryDIV = document.querySelector(`#gallery`);
-const modalContainer = document.querySelector(`.modal-container`);
 const modalBtn = document.querySelector(`#modal-close-btn`);
 const employeeList = [];
 let employees;
@@ -61,6 +60,37 @@ const generateHTML = (employee) => {
     galleryDIV.insertAdjacentHTML(`beforeend`, galleryHTML);
 };
 
+
+/**
+ * function to reformat employees mobile number
+ * 
+ * @param {text} number phone number
+ * @return {text} formatted number
+ */
+const formatPhoneNumber = (number) => {
+    //Filter only numbers from the input
+    let cleaned = ('' + number).replace(/\D/g, '');
+    
+    //Check if the input is of correct
+    let match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
+    if (match) {
+      //Remove the matched extension code
+      //Change this to format for any country code.
+      let intlCode = (match[1] ? '+1 ' : '')
+      return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('')
+    }    
+    return null;
+}
+
+
+/**
+ * function to reformat date of birth
+ * 
+ * @param {text} date ISO date format
+ * @return {text} formatted date
+ */
+const formattedDate = (date) => new Date(date).toLocaleDateString();
+
 /**
  * function to generate select employee modal with 
  * additional details of employee
@@ -68,6 +98,7 @@ const generateHTML = (employee) => {
  * @param {object} employee object details
  */
 const generateModalHTML = (employee) => {
+    console.log(employee);
     modalHTML = `
         <div class="modal-container">
             <div class="modal">
@@ -78,9 +109,9 @@ const generateModalHTML = (employee) => {
                     <p class="modal-text">${employee.email}</p>
                     <p class="modal-text cap">${employee.location.city}</p>
                     <hr>
-                    <p class="modal-text">(555) 555-5555</p>
+                    <p class="modal-text">${formatPhoneNumber(employee.phone)}</p>
                     <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
-                    <p class="modal-text">Birthday: 10/21/2015</p>
+                    <p class="modal-text">Birthday: ${formattedDate(employee.dob.date)}</p>
                 </div>
             </div>
         </div>`;
@@ -98,12 +129,18 @@ galleryDIV.addEventListener(`click`, e =>{
     const eventPath = e.composedPath();
     //this will let you get the card that was clicked no matter what element they click on specifically
     const employeeCard = eventPath[eventPath.indexOf(galleryDIV) - 1];
+    // const employeeModal = eventPath[eventPath.indexOf(galleryDIV) - 1];
+    // console.log(employeeModal);
     if (employeeCard) {
       //this will provide a live list of the cards which you can reference when you create the modal. The indexes of these
       const cardsArray = [...document.getElementsByClassName("card")];
       const indexOfEmployeeCard = cardsArray.indexOf(employeeCard);
       generateModalHTML(employeeList[indexOfEmployeeCard]);
-    }
+    } //else if (employeeModal.getAttribute(`id`) === `modal-close-btn`) {
+    //     const modalContainer = document.querySelector(`.modal-container`);
+    //     console.log(modalContainer);
+    //     modalContainer.style.display = `none`;
+    // }
 });
 
 
